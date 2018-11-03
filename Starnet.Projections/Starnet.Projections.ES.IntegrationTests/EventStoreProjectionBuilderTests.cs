0d79958024cfc2a26303aa8fbe76b6540a8c6f0c
Zvjezdan Tomicevic <zeko77@gmail.com>
2018-11-03 21:22:18 +0100

    EventStoreProjectionBuilder

---
+++ Starnet.Projections/Starnet.Projections.ES.IntegrationTests/EventStoreProjectionBuilderTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Starnet.Projections.ES.IntegrationTests
+{
+    [TestFixture]
+    class EventStoreProjectionBuilderTests
+    {
+        [Test]
+        public void DevicesProjection_is_created()
+        {
+            var p = new EventStoreProjectionParameters {
+                Name = "ProjectionDevices",
+                SourceStreamNames = new List<string> { "$ce-Locations" },
+                DestinationStreamName = "cp-Devices",
+                EventsToInclude = new Type[] { typeof(LocationOpened) }
+            };
+            var proj = EventStoreProjectionBuilder.BuildProjectionDefinition(p);
+           
+            string expected = "fromStreams('$ce-Locations').when({LocationOpened: function(s,e){linkTo('cp-Devices', e);return s;}})";
+            Assert.That(proj.Name, Is.EqualTo("ProjectionDevices"));
+            Assert.That(proj.Source, Is.EqualTo(expected));
+        }
+
+        class LocationOpened
+        {
+        }
+    }
+}
---
+++ Starnet.Projections/Starnet.Projections.ES/EventStoreProjection.cs
@@ -0,0 +1,8 @@
+﻿namespace Starnet.Projections.ES
+{
+    public class EventStoreProjection
+    {
+        public string Name { get; set; }
+        public string Source { get; set; }
+    }
+}
