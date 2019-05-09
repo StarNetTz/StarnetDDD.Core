6af0b63395e84671f16ba2a0734341ce3b487330
Zvjezdan Tomicevic <zeko77@gmail.com>
2019-05-09 14:15:46 +0200

    Added concurrency exception

---
+++ Starnet.Aggregates/Starnet.Aggregates.Tests/ConcurrencyExceptionTEsts.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Starnet.Aggregates.Tests
+{
+    class ConcurrencyExceptionTEsts
+    {
+        [Test]
+        public void CanInstantiate()
+        {
+            var d = new ConcurrencyException();
+            var d2 = new ConcurrencyException("error", d);
+        }
+    }
+}
