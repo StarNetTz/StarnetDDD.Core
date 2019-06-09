0f76b5fc95ebcfa3887dcc3fe752479f5fb02f0d
Selvin Fehric <selvin.fehric@gmail.com>
2019-06-09 22:35:45 +0200

    project templates updated - dot net core 2.2 + all nuget packages upgraded

--- VisualStudioTemplates/StarnetDomainProject/DomainName.ReadModel.Projections.Tests/OrganizationProjectionTests/OrganizationRegisteredTests.cs
+++ VisualStudioTemplates/StarnetDomainProject/DomainName.ReadModel.Projections.Tests/OrganizationProjectionTests/OrganizationRegisteredTests.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace $safeprojectname$
 {
     [TestFixture]
-    public class OrganizationRegisteredTests : ProjectionSpecification<OrganizationProjection, Organization>
+    public class OrganizationRegisteredTests : ProjectionSpecification<OrganizationProjection>
     {
         string Id;
 
@@ -20,8 +20,8 @@ namespace $safeprojectname$
         [Test]
         public async Task can_project_event()
         {
-            await Given(new OrganizationRegistered() { Id = Id, Name = "Betting Shop Royal", Address = new PL.Address { Street = "My street", City = "My City", Country = "My Country", State = "TK", ZipCode = "75000" }});
-            await Expect(new Organization() { Id = Id, Name = "Betting Shop Royal", Address = new PL.Address { Street = "My street", City = "My City", Country = "My Country", State = "TK", ZipCode = "75000" }});
+            await Given(new OrganizationRegistered() { Id = Id, Name = "Betting Shop Royal", Address = new PL.Address { Street = "My street", City = "My City", Country = "My Country", State = "TK", ZipCode = "75000" } });
+            await Expect(new Organization() { Id = Id, Name = "Betting Shop Royal", Address = new PL.Address { Street = "My street", City = "My City", Country = "My Country", State = "TK", ZipCode = "75000" } });
         }
     }
 }
