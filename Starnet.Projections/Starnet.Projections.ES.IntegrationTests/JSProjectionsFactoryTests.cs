ec273903187b954e7fecb1f3d631407ca700f148
Zvjezdan Tomicevic <zeko77@gmail.com>
2020-02-19 09:38:32 +0100

    Refactored LoggerFactory creation

--- Starnet.Projections/Starnet.Projections.ES.IntegrationTests/JSProjectionsFactoryTests.cs
+++ Starnet.Projections/Starnet.Projections.ES.IntegrationTests/JSProjectionsFactoryTests.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using NLog;
 using NLog.Extensions.Logging;
 using NUnit.Framework;
@@ -13,20 +12,27 @@ namespace Starnet.Projections.ES.IntegrationTests
         [Test]
         public async Task CanCreateProjectionUsingSimpleInjector()
         {
-            var container = new Container();
-            var loggerFactory = new LoggerFactory();
-            loggerFactory.AddNLog(new NLogProviderOptions { CaptureMessageTemplates = true, CaptureMessageProperties = true });
-            LogManager.LoadConfiguration("nlog.config");
-            container.Register<ILoggerFactory>(() => loggerFactory, Lifestyle.Singleton);
-
-            container.Register<JSProjectionsFactory>();
-
-            container.RegisterConditional( typeof(Microsoft.Extensions.Logging.ILogger),c => typeof(Logger<>).MakeGenericType(c.Consumer.ImplementationType), Lifestyle.Singleton, c => true);
-            container.Verify();
-
+            var container = CreateDIContainer();
             var fact = container.GetInstance<JSProjectionsFactory>();
             fact.Projections.Add("AssociationsOverviewWithNlog", "fromStreams('$ce-Competitions').when({CompetitionAdded: function(s,e){linkTo('cp-AssociationsOverview', e);return s;},CompetitionRenamed: function(s,e){linkTo('cp-AssociationsOverview', e);return s;},CompetitionsAssociationChanged: function(s,e){linkTo('cp-AssociationsOverview', e);return s;},CompetitionsRankChanged: function(s,e){linkTo('cp-AssociationsOverview', e);return s;}})");
             await fact.CreateProjections();
         }
+
+            static Container CreateDIContainer()
+            {
+                var container = new Container();
+                var loggerFactory = LoggerFactory.Create(builder =>
+                {
+                    builder.AddNLog(new NLogProviderOptions { CaptureMessageTemplates = true, CaptureMessageProperties = true });
+                });
+
+                LogManager.LoadConfiguration("nlog.config");
+                container.Register(() => loggerFactory, Lifestyle.Singleton);
+                container.Register<JSProjectionsFactory>();
+
+                container.RegisterConditional(typeof(Microsoft.Extensions.Logging.ILogger), c => typeof(Logger<>).MakeGenericType(c.Consumer.ImplementationType), Lifestyle.Singleton, c => true);
+                container.Verify();
+                return container;
+            }
     }
 }
