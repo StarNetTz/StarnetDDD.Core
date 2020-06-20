aa3fdcd4782009cee0ba56c1041d910dc5ca304c
Zvjezdan Tomicevic <zeko77@gmail.com>
2020-06-20 09:51:16 +0200

    Done

---
+++ Starnet.Projections/Starnet.Projections.RavenDb.IntegrationTests/RavenConfig.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Starnet.Projections.RavenDb.IntegrationTests
+{
+    public class RavenConfig
+    {
+        public string[] Urls { get; set; }
+        public string DatabaseName { get; set; }
+        public string CertificateFilePath { get; set; }
+        public string CertificateFilePassword { get; set; }
+
+        public static RavenConfig FromConfiguration(IConfiguration conf)
+        {
+            return new RavenConfig
+            {
+                Urls = conf["RavenDb:Urls"].Split(';'),
+                CertificateFilePassword = conf["RavenDb:CertificatePassword"],
+                CertificateFilePath = conf["RavenDb:CertificatePath"],
+                DatabaseName = conf["RavenDb:DatabaseName"]
+            };
+        }
+    }
+}
